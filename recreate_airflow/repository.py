482d1401c406766d8dcb0f54a634d18b5333e753
djayatillake <dave665@gmail.com>
2022-09-19 00:42:14 +0100

    fixes to point to airflow package

--- recreate_airflow/repository.py
+++ recreate_airflow/repository.py
@@ -4,17 +4,10 @@ from recreate_airflow.airflow_complex_dag import main_airflow_dag
 
 from dagster import repository
 
-main_airflow_dag = make_dagster_job_from_airflow_dag(main_airflow_dag)
+airflow_main_airflow_dag = make_dagster_job_from_airflow_dag(main_airflow_dag)
 
 @repository
-def with_airflow():
-    return [main_airflow_dag]
+def recreate_airflow():
+    return [airflow_main_airflow_dag]
 
 # end_repo_marker_0
-
-# start_repo_marker_1
-
-airflow_simple_dag_with_execution_date = make_dagster_job_from_airflow_dag(
-    dag=main_airflow_dag, tags={"airflow_execution_date": "2022-09-18 00:00:00"}
-)
-# end_repo_marker_1
