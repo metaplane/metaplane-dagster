71fbbb9bc1762e456d7f2562255b01c02f26f022
david-metaplane <107431950+david-metaplane@users.noreply.github.com>
2022-11-12 16:16:21 +0000

    Update __init__.py

--- fivetran_dbtcloud_simple/assets/__init__.py
+++ fivetran_dbtcloud_simple/assets/__init__.py
@@ -2,8 +2,8 @@ from dagster_fivetran import fivetran_resource, load_assets_from_fivetran_instan
 
 fivetran_instance = fivetran_resource.configured(
     {
-        "api_key": "some_key",
-        "api_secret": "some_secret",
+        "api_key": {"env": "FIVETRAN_API_KEY"},
+        "api_secret": {"env": "FIVETRAN_API_SECRET"},
     }
 )
 fivetran_assets = load_assets_from_fivetran_instance(fivetran_instance)
