1bf25d412ccf828336d258797d8141f83f212cee
david-metaplane <107431950+david-metaplane@users.noreply.github.com>
2022-11-12 23:01:58 +0000

    final fix to schedule

--- my_dagster_project/repository.py
+++ my_dagster_project/repository.py
@@ -1,10 +1,10 @@
 import os
 
-from dagster import define_asset_job, load_assets_from_package_module, repository, with_resources, ScheduleDefinition
+from dagster import define_asset_job, load_assets_from_package_module, repository, with_resources
 
 from my_dagster_project import assets
 from .assets.fivetran_dbt_cloud_simple.fivetran_simple import fivetran_assets
-from .assets.fivetran_dbt_cloud_simple.dbt_cloud_simple import my_dbt_cloud_job
+from .assets.fivetran_dbt_cloud_simple.dbt_cloud_simple import my_dbt_cloud_job, basic_schedule
 
 
 @repository
@@ -46,6 +46,6 @@ def my_dagster_project():
         load_assets_from_package_module(assets),
         define_asset_job(name="all_assets_job"),
         fivetran_assets,
-        my_dbt_cloud_job]
+        my_dbt_cloud_job,
+        basic_schedule]
 
-basic_schedule = ScheduleDefinition(job=my_dbt_cloud_job, cron_schedule="0 0 * * *")
