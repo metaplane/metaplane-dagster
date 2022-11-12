0db66837e9570134362c2d124ad9756405a55312
david-metaplane <107431950+david-metaplane@users.noreply.github.com>
2022-11-12 16:44:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 if __name__ == "__main__":
     setup(
         name="fivetran_dbtcloud_simple",
-        packages=find_packages(exclude=["fivetran_dbt_cloud_simple_tests"]),
+        packages=find_packages(exclude=["fivetran_dbtcloud_simple_tests"]),
         install_requires=[
             "dagster",
             "dagster-fivetran",
