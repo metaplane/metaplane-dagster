c6d722216dc3bd09778ebb2873c52a2451d5e83f
david-metaplane <107431950+david-metaplane@users.noreply.github.com>
2022-11-20 19:08:04 +0000

    creating SDA job

--- my_dagster_project/assets/dbt_core/dbt_core.py
+++ my_dagster_project/assets/dbt_core/dbt_core.py
@@ -1,19 +1,14 @@
-from dagster import job, ScheduleDefinition, file_relative_path, build_op_context
+from dagster import job, ScheduleDefinition, file_relative_path, define_asset_job, AssetSelection
 #from .fivetran_simple import fivetran_assets 
 #let's see if we need this as in theory the sources in the dbt project should get mapped
-from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource, dbt_build_op
+from dagster_dbt import load_assets_from_dbt_project
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../../metaplane-dbt")
 DBT_PROFILES = file_relative_path(__file__, "../../resources")
 
 dbt_assets = load_assets_from_dbt_project(project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES)
 
-dfe_dbt_cli_resource = dbt_cli_resource.configured(
-                {"project_dir": DBT_PROJECT_PATH, "profiles_dir": DBT_PROFILES, "select": "deal_flow_enriched"}
-                )
+dfe_job = define_asset_job(name="dfe_job", selection=AssetSelection.keys("deal_flow_enriched").upstream() |
+    AssetSelection.groups("hubspot"))
 
-@job(resource_defs={"dbt":dfe_dbt_cli_resource}, tags={"type": "DFE_daily"})
-def my_dbt_cli_job():
-    dbt_build_op()
-
-dfe_schedule = ScheduleDefinition(job=my_dbt_cli_job, cron_schedule="0 2 * * *")
+dfe_schedule = ScheduleDefinition(job=dfe_job, cron_schedule="0 2 * * *")
--- my_dagster_project/repository.py
+++ my_dagster_project/repository.py
@@ -5,7 +5,7 @@ from dagster_dbt import dbt_cli_resource
 from my_dagster_project import assets
 from .assets.fivetran_dbt_cloud_simple.fivetran_simple import fivetran_assets
 from .assets.fivetran_dbt_cloud_simple.dbt_cloud_simple import my_dbt_cloud_job, basic_schedule
-from .assets.dbt_core.dbt_core import dbt_assets, DBT_PROFILES, DBT_PROJECT_PATH, my_dbt_cli_job, dfe_schedule
+from .assets.dbt_core.dbt_core import dbt_assets, DBT_PROFILES, DBT_PROJECT_PATH, dfe_job, dfe_schedule
 
 @repository
 def my_dagster_project():
@@ -15,7 +15,7 @@ def my_dagster_project():
         fivetran_assets,
         my_dbt_cloud_job,
         basic_schedule,
-        my_dbt_cli_job,
+        dfe_job,
         dfe_schedule] + with_resources(
         dbt_assets, 
         resource_defs={
