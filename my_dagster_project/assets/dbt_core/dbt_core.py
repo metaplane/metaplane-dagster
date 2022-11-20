91444fd9ac8991138a696a85e438c7b4d1c8bc9d
david-metaplane <107431950+david-metaplane@users.noreply.github.com>
2022-11-20 00:37:59 +0000

    fixing ref to dbt project

--- my_dagster_project/assets/dbt_core/dbt_core.py
+++ my_dagster_project/assets/dbt_core/dbt_core.py
@@ -6,5 +6,4 @@ from dagster_dbt import load_assets_from_dbt_project
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../../metaplane-dbt")
 DBT_PROFILES = file_relative_path(__file__, "../../resources")
 
-dbt_assets = load_assets_from_dbt_project(project_dir="https://github.com/metaplane/metaplane-dbt",
-    profiles_dir=DBT_PROFILES)
+dbt_assets = load_assets_from_dbt_project(project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES)
